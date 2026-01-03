752858535b3720b72705e9294960fae98c4dc3ae
Tanmay <sherkartanmay@gmail.com>
2026-01-03 23:02:17 +0530

    Regression projects

---
+++ Regression/linear_regression/diabetes_model.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+print('Data Set Loaded Successfully')
+diabetes = load_diabetes()
+
+df = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
+
+target = pd.Series(diabetes.target, name='progression')
+
+print('--- HEAD ---')
+print(df.head())
+print('\n--- INFO ---')
+print(df.info())
+print('\n--- DESCRIBE ---')
+print(df.describe())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df, target, test_size=0.2, random_state=42
+)
+
+print(f'\nTraining set: {X_train.shape[0]} rows')
+print(f'Testing set: {X_test.shape[0]} rows')
+
+print('\n---Environment Info---')
+print(f'\nPandas version: {pd.__version__}')
+print(f'NumPy version:  {np.__version__}')
+print(f'Scikit-learn version: {__import__('sklearn').__version__}')
+print(f'Is X_train a DataFrame? {isinstance(X_train, pd.DataFrame)}')
+
+print('\n---Training the Model---')
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print('Model trained successfully.')
+predictions = model.predict(X_test)
+
+print('\nFirst 5 Predictions:', predictions[:5])
+print('First 5 Actual Values:', y_test.values[:5])
+
+print('\n---Evaluating the Model---')
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(y_test, predictions)
+r2 = r2_score(y_test, predictions)
+
+print('Mean Squared Error (MSE):', mse)
+print('Root Mean Squared Error (RMSE):', rmse)
+print('Mean Absolute Error (MAE):', mae)
+print('R-Squared Score:', r2)
+
+print('\n--- Model Performance ---')
+print(f'Mean Squared Error: {mse:.2f}')
+print(f'R-Squared Score:   {r2:.2f}')
+
+print('\n--- Feature Importance ---')
+weights = pd.Series(model.coef_, index=diabetes.feature_names)
+
+print(weights.sort_values(ascending=False))
